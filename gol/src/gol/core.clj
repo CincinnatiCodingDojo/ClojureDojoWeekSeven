8842b567425eaff07ff0fb910e3b3ec92798aab3
Kyle Hodgson <kyle.hodgson@gmail.com>
2013-08-26 13:00:54 -0400

    Second commit

--- gol/src/gol/core.clj
+++ gol/src/gol/core.clj
@@ -1,9 +1,32 @@
 (ns gol.core
   (:gen-class))
 
-(defn -main
-  "I don't do a whole lot ... yet."
-  [& args]
-  ;; work around dangerous default behaviour in Clojure
-  (alter-var-root #'*read-eval* (constantly false))
-  (println "Hello, World!"))
+(defn count-live-cells [board]
+	(reduce + (flatten board)))
+
+
+(defn get-cell [board row column]
+	(nth (nth board row []) column 0)
+)
+
+(defn get-neighborhood [board row column]	
+	(for [y (range (- row 1)   (+ row 2))
+		  x (range (- column 1) (+ column 2))
+          :when (not (and (= y row) (= x column)))]
+		  [y x])
+)
+
+(defn sum-neighborhood [board row column]
+	(reduce +
+	(map #(get-cell board (% 0) (% 1))
+		(get-neighborhood board row column)))
+)
+
+(defn next-gen [board row column]
+	(let [live-neighbors (sum-neighborhood board row column)]
+		(nth [0 0 (get-cell board row column) 1 0 0 0 0] live-neighbors )
+		)
+
+)
+
+
--- gol/test/gol/core_test.clj
+++ gol/test/gol/core_test.clj
@@ -1,7 +1,42 @@
 (ns gol.core-test
   (:require [clojure.test :refer :all]
             [gol.core :refer :all]))
+(def board
+	[[0 1 1 1]
+	 [1 0 1 0]
+	 [0 1 0 1]
+	 [1 1 1 0]])
 
-(deftest a-test
-  (testing "FIXME, I fail."
-    (is (= 0 1))))
+
+(deftest can-count-live-cells
+	(is (= 
+		(count-live-cells board)
+		10))
+)
+
+(deftest can-get-neighborhood
+	(is (= (get-neighborhood board 1 1) [[0 0] [0 1] [0 2] [1 0] [1 2] [2 0] [2 1] [2 2]]))
+)
+
+(deftest can-get-cell
+	(is (= (get-cell board 0 1) 1 ))
+	(is (= (get-cell board 2 3) 1 ))
+	(is (= (get-cell board 0 2) 1 ))
+	(is (= (get-cell board 2 0) 0 ))
+	(is (= (get-cell board 0 0) 0 ))
+	(is (= (get-cell board -1 0) 0 ))
+	(is (= (get-cell board 0 -1) 0 ))
+	(is (= (get-cell board 5 0) 0 ))
+	(is (= (get-cell board 0 5) 0 ))
+)
+
+(deftest can-sum-neighborhood
+	(is (= (sum-neighborhood board 1 1) 5)))
+
+(deftest test-next-gen
+	(is (= 0 (next-gen board 1 1)))
+	(is (= 0 (next-gen board 3 3)))
+	(is (= 1 (next-gen board 3 1)))
+	(is (= 1 (next-gen board 3 0)))
+	(is (= 1 (next-gen board 0 2)))
+)
